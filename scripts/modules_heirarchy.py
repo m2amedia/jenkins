78d707914aa39893986684ea700495c8558599cf
Sam Bagnall <sam.bagnall@m2amedia.tv>
2019-04-15 15:36:48 +0100

    NO-TICKET added a timeout to build arifact script when pipenv installing

--- scripts/modules_heirarchy.py
+++ scripts/modules_heirarchy.py
@@ -7,28 +7,35 @@ import json
 from collections import deque
 
 parser = argparse.ArgumentParser(description="Create a release file")
-parser.add_argument('--e', help="Space separated list of external dependencies (eg ci environments rely on networking)", nargs="*")
+parser.add_argument('--e', help="Space separated list of external dependencies (eg ci environments rely on networking)",
+                    nargs="*")
 parser.add_argument('--target', help="Path to modules.json file")
 args = parser.parse_args()
 external_dependencies = args.e
 target = args.target
 
-data = {}
+data = {'external_dependencies': []}
 if external_dependencies:
     data['external_dependencies'] = external_dependencies
-
+origins = []
 modules = {}
 for dir_path, _, filenames in os.walk('.'):
     for filename in [f for f in filenames if f == 'remote_states.tf']:
         with open(os.path.join(dir_path, filename), "r") as my_file:
-            outputs = hcl.load(my_file)
+            try:
+                outputs = hcl.load(my_file)
+            except ValueError as ve:
+                continue
             for output in outputs["data"].values():
                 module_name = os.path.basename(dir_path)
-                pipeline = dir_path.replace("./", '').replace("/", "-")
-                modules[module_name] = []
-                for key in output.keys():
-                    if key not in data['external_dependencies']:
-                        modules[module_name].append(key)
+                if '/origins/' in dir_path:
+                    origins.append(module_name)
+                else:
+                    pipeline = dir_path.replace("./", '').replace("/", "-")
+                    modules[module_name] = []
+                    for key in output.keys():
+                        if key not in data['external_dependencies']:
+                            modules[module_name].append(key)
 
 
 def sort_modules(modules_to_sort):
@@ -40,7 +47,6 @@ def sort_modules(modules_to_sort):
         state[node] = False
         for dependency in modules_to_sort.get(node, ()):
             key_state = state.get(dependency, None)
-            print("key_state", key_state)
             if key_state is False:
                 raise ValueError("cycle")
             if key_state is True:
@@ -58,6 +64,7 @@ def sort_modules(modules_to_sort):
 
 
 data['order'] = list(sort_modules(modules))
+data['origins'] = origins
 
 with open(target, "w") as new_file:
     json.dump(data, new_file, indent=4)
